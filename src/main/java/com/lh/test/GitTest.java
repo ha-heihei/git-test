a026482598ace652efa3b0b7fd6b129cba749461
lihao <lihao2@eastmoney.com>
2022-08-25 14:10:31 +0800

    项目初始化

---
+++ src/main/java/com/lh/test/GitTest.java
@@ -0,0 +1,16 @@
+package com.lh.test;
+
+/**
+ * @author: LiHao
+ * @date: 2022/8/25 14:04
+ * @email lihao2@eastmoney.com
+ * @Version: 1.0
+ * @Description:
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("init");
+    }
+
+}
